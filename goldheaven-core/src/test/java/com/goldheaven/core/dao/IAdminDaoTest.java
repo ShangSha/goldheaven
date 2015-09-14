667429818a12117c24391c1071a64352b53ad175
lizhenqiang <lizhenqiang@meizu.com>
2015-09-14 16:36:37 +0800

    提交单元测试
    Change-Id: I6062e6b3a944c4fa7c8d515485fd58ea64760fcc
    Signed-off-by: lizhenqiang <lizhenqiang@meizu.com>

---
+++ goldheaven-core/src/test/java/com/goldheaven/core/dao/IAdminDaoTest.java
@@ -0,0 +1,65 @@
+/**
+ * Project: goldheaven-core
+ * 
+ * File Created at 2015-9-14
+ * $Id$
+ * 
+ */
+package com.goldheaven.core.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * TODO Comment of IAdminDaoTest
+ * @author lizhenqiang
+ *
+ */
+public class IAdminDaoTest {
+
+    /**
+     * Test method for {@link com.goldheaven.core.dao.IAdminDao#getUserByNameAndPW(java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public void testGetUserByNameAndPW() {
+        fail("Not yet implemented");
+    }
+
+
+    /**
+     * Test method for {@link com.goldheaven.core.dao.IAdminDao#getAdminList(java.lang.Integer, java.lang.Integer)}.
+     */
+    @Test
+    public void testGetAdminList() {
+        fail("Not yet implemented");
+    }
+
+
+    /**
+     * Test method for {@link com.goldheaven.core.dao.IAdminDao#getAdminNum()}.
+     */
+    @Test
+    public void testGetAdminNum() {
+        fail("Not yet implemented");
+    }
+
+
+    /**
+     * Test method for {@link com.goldheaven.core.dao.IAdminDao#saveAdmin(com.goldheaven.core.entity.AdminInfo)}.
+     */
+    @Test
+    public void testSaveAdmin() {
+        fail("Not yet implemented");
+    }
+
+
+    /**
+     * Test method for {@link com.goldheaven.core.dao.IAdminDao#deleteAdmin(java.lang.Long)}.
+     */
+    @Test
+    public void testDeleteAdmin() {
+        fail("Not yet implemented");
+    }
+
+}
